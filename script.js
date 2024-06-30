ef075acf3d6ef750b92f3d6f4c7e1ff5aa7d58fb
kitonde <omunenevevo@gmail.com>
2024-06-30 16:36:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle navigation menu on mobile
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    document.querySelector('.nav').classList.toggle('active');
+  });
+  
